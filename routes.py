da50a39092182870b29a9b6698c320d7c216d629
Kondou Kazuki <zaku2590@gmail.com>
2025-04-18 11:04:50 +0900

    修正

--- routes.py
+++ routes.py
@@ -270,7 +270,7 @@ def blog_list():
 @main_bp.route("/blog/<int:post_id>")
 def blog_detail(post_id):
     post = BlogPost.query.get_or_404(post_id)
-    recent_posts = BlogPost.query.order_by(BlogPost.updated_at.desc()).limit(15)
+    recent_posts = BlogPost.query.order_by(BlogPost.updated_at.desc()).limit(5)
     return render_template("blog_detail.html", post=post, recent_posts=recent_posts)
 
 @main_bp.route("/blog/new", methods=["GET", "POST"])
