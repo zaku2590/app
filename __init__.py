19b3026fb0427ea554a8cc5948532307790a2739
kondou_kazuki <kondou_kazuki@plise.co.jp>
2025-04-30 13:59:20 +0900

    再修正

--- __init__.py
+++ __init__.py
@@ -9,28 +9,21 @@ from flask import Flask
 from authlib.integrations.flask_client import OAuth
 from flask_sqlalchemy import SQLAlchemy
 
-# SQLAlchemy: 再接続設定を有効化
+# SQLAlchemy: 再接続サポート付きインスタンス
 db = SQLAlchemy(engine_options={"pool_pre_ping": True})
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object("app.config.Config")
 
-    # DB初期化（再接続サポート付き）
     db.init_app(app)
-
-    # セッションの有効期限（1年）
     app.permanent_session_lifetime = timedelta(days=365)
 
-    # Twitter OAuth設定
     oauth = OAuth(app)
-    twitter_key = os.getenv("TWITTER_API_KEY")
-    twitter_secret = os.getenv("TWITTER_API_SECRET")
-
     twitter = oauth.register(
         name='twitter',
-        client_id=twitter_key,
-        client_secret=twitter_secret,
+        client_id=os.getenv("TWITTER_API_KEY"),
+        client_secret=os.getenv("TWITTER_API_SECRET"),
         request_token_url='https://api.twitter.com/oauth/request_token',
         access_token_url='https://api.twitter.com/oauth/access_token',
         authorize_url='https://api.twitter.com/oauth/authorize',
@@ -38,11 +31,9 @@ def create_app():
     )
     app.twitter = twitter
 
-    # Blueprint登録
     from app.routes import main_bp
     app.register_blueprint(main_bp)
 
-    # リクエスト終了時にDBセッションを解放
     @app.teardown_appcontext
     def shutdown_session(exception=None):
         db.session.remove()
